f58b303fb4206cc23bb88aff88c6dd7948100d86
Fred Frigo <54369199+FrigoF@users.noreply.github.com>
2019-11-19 10:19:12 -0600

    Add files via upload

---
+++ example9_10.m
@@ -0,0 +1,22 @@
+% example9_10 - Example 9.10 Blandford p 534
+% EECE 4510 / 5510 - Marquette University
+% Fred J. Frigo, Ph.D.
+%  
+% Nov 2019
+%
+fs = 11025;T = 1/fS;
+nT = [0:11024]*T;
+
+% Generate Chirp Signal
+s = chirp(nT, 100, 1, fs/2);
+
+% Use equalizer1 and equalizer2
+Alow = 2; fc1 = 2000;-Aclow = 1 + l/sqrt(2);
+[num1 den1] = equalizer1(1, Alow, fs, fc1, Aclow);
+Ahi = 0.2;bw = 600;f0 = 3500;
+Achi = 0.2 + .8/sqrt(2);
+[num2 den2] = equalizer2(f0, Ahi, bw, fs, Achi);
+
+% Filter the original signal
+s1 = filter(numl, deni, s);
+s2 = filter(num2, den2, s2);
