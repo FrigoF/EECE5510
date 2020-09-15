b68a37847ba44e344482218bc16e6729f3aa9686
Fred Frigo <54369199+FrigoF@users.noreply.github.com>
2020-09-15 09:13:40 -0500

    Add files via upload

---
+++ prob_ex3_13.m
@@ -0,0 +1,28 @@
+% Problem example 3.13 - p 159 Blandford and Parr
+%
+% F. Frigo  15-Sep-2020
+%
+sample_period=0.001;
+N = 256;
+t=[0:sample_period:(N-1)*sample_period];
+hz=linspace(-0.25/sample_period, 0.25/sample_period, N);
+w=hz*2*pi;
+
+x=cos(100*t);
+
+figure();
+plot(t, x);
+ylabel('Input signal');
+xlabel('msec');
+
+X=fft(x,N);
+XM=fftshift(X);
+figure();
+plot(hz, abs(XM));
+ylabel('Magnitude');
+xlabel('freq in hz');
+
+figure();
+plot(w, abs(XM));
+ylabel('Magnitude');
+xlabel('freq in rad/sec');
