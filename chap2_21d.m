42345a8c324791539c7b6375fa6783ff37a67ade
Fred Frigo <54369199+FrigoF@users.noreply.github.com>
2022-09-06 23:33:00 -0500

    Add files via upload

--- chap2_21d.m
+++ chap2_21d.m
@@ -1,15 +1,18 @@
 % chap2_21d.m  - Prob 2.21 part d - Blandford
 % EECE 4510 / 5510 - Marquette University
 % Fred J. Frigo
-% Sep - 2019
+% Sep - 2019, 2022
 % 
+% y[n] = 5y[n-1] + y[n-2] + 3x[n-1] -4x[n-2]
+% y[n]  -5y[n-1] - y[n-2] = 3x[n-1] -4x[n-2]
+% Y(z)(1 - 5z^-1 -z^-2) = X(z)(3z^-1 - 4z^-2)
 % Y(z)/X(z) = (3z^-1 - 4z^-2) / (1 - 5z^-1 -z^-2)
 
-num = [0  3 -4];
-den = [1 -5 -1];
+num = [0.0  3.0 -4.0];
+den = [1.0 -5.0 -1.0];
 
-
-x= ones(1,31); % unit step
+x= zeros(1,31); 
+x(1)=1; % System is BIBO stable if the impulse response goes to zero with time.
 k=0:30; % x-axis for plot
 
 y = filter( num, den, x); 
@@ -24,3 +27,4 @@ Ts = 0.01;
 sys = tf(num, den, Ts);
 
 linearSystemAnalyzer(sys);
+
