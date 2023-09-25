5d15af79f7f36c6972ce127c476e1904adf957fe
Fred Frigo <54369199+FrigoF@users.noreply.github.com>
2023-09-25 18:27:31 -0500

    Add files via upload

---
+++ example_5_2.m
@@ -0,0 +1,27 @@
+% Example 5.2 - p 211 Blandford and Parr
+% 
+% F. Frigo  20-Sep-2022
+% Marquette University
+%
+N = 30; %Order
+fs = 11025;
+F = [0 2000/(fs/2) 2000/(fs/2) 1]; %Frequency vector normalized
+%to fs/2
+M = [2 2 0 0]; %Corresponding magnitude vector
+num = firls(N, F, M);
+[H f] = freqz(num, 1, 1024, fs) ;
+figure (1) ;
+subplot(2, 1, 1)
+plot(f, abs(H));
+axis([0 fs/2 0 3]);
+xlabel ('frequency in Hz ');
+ylabel ('gain');
+title('FIR filter from Fourier series');
+subplot(2, 1, 2);
+plot(f, unwrap(angle(H)*180/pi));
+axis ([0 fs/2 -200 200]);
+xlabel ('frequency in H z ');
+ylabel ('Phase in degrees');
+figure (2); clf ;
+zplane(num, 1) ;
+title ('z-plane plot');
