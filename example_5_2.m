7db40d884aa9cee09a55af7eeb65299e3897cbc5
Fred Frigo <54369199+FrigoF@users.noreply.github.com>
2023-09-25 19:54:19 -0500

    Update example_5_2.m

--- example_5_2.m
+++ example_5_2.m
@@ -6,13 +6,13 @@
 N = 30; %Order
 fs = 11025;
 F = [0 2000/(fs/2) 2000/(fs/2) 1]; %Frequency vector normalized to fs/2
-M = [2 2 0 0]; %Corresponding magnitude vector
+M = [1 1 0 0]; %Corresponding magnitude vector
 num = firls(N, F, M);
 [H f] = freqz(num, 1, 1024, fs) ;
 figure (1) ;
 subplot(2, 1, 1)
 plot(f, abs(H));
-axis([0 fs/2 0 3]);
+axis([0 fs/2 0 1.5]);
 xlabel ('frequency in Hz ');
 ylabel ('gain');
 title('FIR filter from Fourier series');
