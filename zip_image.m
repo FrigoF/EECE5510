0991b47102132da1e833ea1094e2a107a824e075
Fred Frigo <54369199+FrigoF@users.noreply.github.com>
2023-10-03 15:00:28 -0500

    Add files via upload

--- zip_image.m
+++ zip_image.m
@@ -34,12 +34,20 @@ zip_row = row*zip;
 zip_col = col*zip;
 
 if zip < 1 
-    zip_buffer = F(1:row, 1:col);
-    zip_scale = 1;
+    a = row/2 - zip_row/2;
+    b = row/2 + zip_row/2;
+    c = col/2 - zip_col/2;
+    d = col/2 + zip_col/2;
+    reduce_buffer = S(a:b, c:d);
+    zip_buffer = fftshift(reduce_buffer);
 else
-    zip_buffer = zeros( zip_row, zip_col);
-    zip_buffer(1:row, 1:col) = F;
-    zip_scale = 2;
+    a = zip_row/2 - row/2;
+    b = zip_row/2 + row/2 - 1;
+    c = zip_col/2 - col/2;
+    d = zip_col/2 + col/2 - 1;
+    interpolation_buffer = zeros( zip_row, zip_col);
+    interpolation_buffer(a:b, c:d) = S;
+    zip_buffer = fftshift(interpolation_buffer);
 end
 
 zip_complex = ifft2(zip_buffer);
@@ -48,6 +56,6 @@ zip_max = max(max(zip_mag_image));
 zip_gray = (zip_mag_image/zip_max);
 
 figure();
-imshow(zip_gray*zip_scale);
+imshow(zip_gray);
 resize_title = sprintf("Resized Gray Scale image: %d x %d", zip_row, zip_col);
 title(resize_title);
