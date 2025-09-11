c6a5c3c50106948bbb28e2d8053e8b1912bc3411
Fred Frigo <54369199+FrigoF@users.noreply.github.com>
2025-09-11 09:03:40 -0500

    Add files via upload

---
+++ example3_13.m
@@ -0,0 +1,42 @@
+% Example 3.13 - p 119 Blandford and Parr
+%
+% F. Frigo  15-Sep-2020
+%
+sample_period=0.001;
+N = 256+1; 
+t= 0:sample_period:(N-1)*sample_period;
+hz=linspace(-0.5/sample_period, 0.5/sample_period, N);
+w=hz/2*pi;
+
+w0 = 100.0;
+magnitude = 1.0;
+x = magnitude*cos(w0*t);
+
+figure();
+plot(t, x);
+ylabel('Input signal');
+xlabel('msec');
+
+X=fft(x,N);
+XM=fftshift(X);
+figure();
+plot(hz, abs(XM));
+ylabel('Magnitude');
+xlabel('freq in hz');
+
+figure();
+plot(w, abs(XM));
+ylabel('Magnitude');
+xlabel('freq in rad/sec');
+
+% Analytic solution using table 3.3 on page 96
+XM_ideal = zeros(N);
+offset = (N+1)/2; 
+w0_index = round(w0/(8*pi));
+XM_ideal(offset - w0_index) = magnitude*pi; 
+XM_ideal(offset + w0_index) = magnitude*pi;
+figure();
+plot(w,abs(XM_ideal));
+ylabel('Ideal Magnitude');
+xlabel('freq in rad/sec');
+
